a523b4c40c6a42c4f8c39d3a624d47a11de82861
nguyencaotai1969 <nguyencaotai1969>
2020-12-02 13:16:22 +0700

    caotai

--- View/Admin/user.php
+++ View/Admin/user.php
@@ -2,43 +2,73 @@
 <html lang="en">
 
 <head>
- <meta charset="UTF-8">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>Document</title>
- <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
- <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
- <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
- <!------ Include the above in your HEAD tag ---------->
- <link rel="stylesheet" href="css/index.css">
+
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ <meta name="description" content="">
+ <meta name="author" content="">
+
+ <title>SB Admin 2 - Dashboard</title>
+
+ <!-- Custom fonts for this template-->
+ <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+ <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+ <!-- Custom styles for this template-->
+ <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
 </head>
 
-<body>
- <?php
- require self::VIEW_FOLDER_NAME . '/Layout/' . str_replace('.', '/', 'header') . '.php';
+<body id="page-top">
 
- ?>
- <div class="container-fluid main-container">
+ <!-- Page Wrapper -->
+ <div id="wrapper">
+
+  <!-- Sidebar -->
   <?php
-  require self::VIEW_FOLDER_NAME . '/Layout/' . str_replace('.', '/', 'left') . '.php';
-
+  require(self::VIEW_FOLDER_NAME . '/Layout/' . str_replace('.', '/', 'left') . '.php');
   ?>
-  <div class="col-md-10 content">
-   <div class="panel panel-default">
-    <div class="panel-heading">
-     Dashboard
+  <!-- End of Sidebar -->
+
+  <!-- Content Wrapper -->
+  <div id="content-wrapper" class="d-flex flex-column">
+
+   <!-- Main Content -->
+   <div id="content">
+
+    <!-- Topbar -->
+    <?php
+    require(self::VIEW_FOLDER_NAME . '/Layout/' . str_replace('.', '/', 'header') . '.php');
+    ?>
+    <!-- End of Topbar -->
+
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+     <!-- Noi Dung Viet o day -->
+
     </div>
-    <div class="panel-body">
-     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
+    <!-- /.container-fluid -->
+
    </div>
+   <!-- End of Main Content -->
+
+   <!-- Footer -->
+   <?php
+   require(self::VIEW_FOLDER_NAME . '/Layout/' . str_replace('.', '/', 'footer') . '.php');
+   ?>
+   <!-- End of Footer -->
+
   </div>
+  <!-- End of Content Wrapper -->
 
  </div>
+ <!-- End of Page Wrapper -->
+
+ <!-- Scroll to Top Button-->
+ <?php
+ require(self::VIEW_FOLDER_NAME . '/Layout/' . str_replace('.', '/', 'lib') . '.php');
+ ?>
 </body>
 
 </html>
