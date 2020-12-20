e0a074095b652fb04cf40a431ecf5d90506b12c6
nguyencaotai1969 <nguyencaotai1969>
2020-12-20 09:07:37 +0700

    caotai

--- View/index/index.php
+++ View/index/index.php
@@ -5,6 +5,7 @@ if (isset($_SESSION["btnsubmit"])) {
 }
 
 ?>
+       
 
 <!DOCTYPE html>
 <html lang='en'>
@@ -22,72 +23,114 @@ if (isset($_SESSION["btnsubmit"])) {
 <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js'></script>
 <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js'></script>
 <style>
-  .login-popup-heading>h4 {
-    color: #258b47;
-    font-size: 18px;
-    line-height: 30px;
-  }
-
-  .new_reg_popup,
-  .new_login_popup {
-    border-radius: 2px;
-    min-height: 332px;
-    width: 500px;
-    margin: 0px auto;
-  }
-
-  .login-popup-btn {
-    background: #258b47;
-    border: none;
-    border-radius: 25px;
-    color: #fff;
-    display: block;
-    font-size: 18px;
-    height: 38px;
-    line-height: 28px;
-    margin: 20px auto 5px;
-    width: 150px;
-    -webkit-transition: all 700ms ease;
-    -moz-transition: all 700ms ease;
-    -ms-transition: all 700ms ease;
-    -o-transition: all 700ms ease;
-  }
-
-  a {
-    color: #258b47;
-    font-size: 18px;
-  }
+@import url("//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css");
+.login-block{
+    background: #DE6262;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to bottom, #FFB88C, #DE6262);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to bottom, #FFB88C, #DE6262); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+float:left;
+width:100%;
+padding : 50px 0;
+}
+.banner-sec{background:url(https://static.pexels.com/photos/33972/pexels-photo.jpg)  no-repeat left bottom; background-size:cover; min-height:500px; border-radius: 0 10px 10px 0; padding:0;}
+.container{background:#fff; border-radius: 10px; box-shadow:15px 20px 0px rgba(0,0,0,0.1);}
+.carousel-inner{border-radius:0 10px 10px 0;}
+.carousel-caption{text-align:left; left:5%;}
+.login-sec{padding: 50px 30px; position:relative;}
+.login-sec .copy-text{position:absolute; width:80%; bottom:20px; font-size:13px; text-align:center;}
+.login-sec .copy-text i{color:#FEB58A;}
+.login-sec .copy-text a{color:#E36262;}
+.login-sec h2{margin-bottom:30px; font-weight:800; font-size:30px; color: #DE6262;}
+.login-sec h2:after{content:" "; width:100px; height:5px; background:#FEB58A; display:block; margin-top:20px; border-radius:3px; margin-left:auto;margin-right:auto}
+.btn-login{background: #DE6262; color:#fff; font-weight:600;}
+.banner-text{width:70%; position:absolute; bottom:40px; padding-left:20px;}
+.banner-text h2{color:#fff; font-weight:600;}
+.banner-text h2:after{content:" "; width:100px; height:5px; background:#FFF; display:block; margin-top:20px; border-radius:3px;}
+.banner-text p{color:#fff;}
 </style>
 
 <body>
-  <div class="login-popup-wrap new_login_popup">
-    <div class="login-popup-heading text-center">
-      <h4><i class="fa fa-lock" aria-hidden="true"></i> Admin </h4>
-    </div>
-    <!--<form accept-charset="utf-8" method="post" action="">-->
-    <form id="loginMember" role="form" action="" method="post">
-      <div class="form-group">
-        <input type="text" class="form-control" id="user_id" placeholder="e-mail / Mobile no" name="username">
-      </div>
-      <div class="form-group">
-        <input type="password" class="form-control" id="password" placeholder="Password" name="password">
-        <?php
+
+<section class="login-block">
+    <div class="container">
+  <div class="row">
+    <div class="col-md-4 login-sec">
+        <h2 class="text-center">Login Now</h2>
+        <form class="login-form" method="post">
+  <div class="form-group">
+    <label for="exampleInputEmail1" class="text-uppercase">Username</label>
+    <input type="text" class="form-control" name="username" placeholder="">
+    
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1" class="text-uppercase">Password</label>
+    <input type="password" name="password" class="form-control" placeholder="">
+  </div>
+  
+  <?php
           if (isset($error)) {
             echo "<b style='color:red'>" . $error . '</b>';
           }
 
         ?>
-      </div>
-      <button type="submit" class="btn btn-default login-popup-btn" name="btnsubmit" value="1">Login</button>
-    </form>
 
+    <div class="form-check">
+    <label class="form-check-label">
+      <input type="checkbox" class="form-check-input">
+      <small>Remember Me</small>
+    </label>
+    <button type="submit" name="btnsubmit" class="btn btn-login float-right">Submit</button>
   </div>
+  
+</form>
+<div class="copy-text">Created with <i class="fa fa-heart"></i> by <a href="http://grafreez.com">Grafreez.com</a></div>
+    </div>
+    <div class="col-md-8 banner-sec">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                 <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                  </ol>
+            <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <img class="d-block img-fluid" src="https://static.pexels.com/photos/33972/pexels-photo.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="banner-text">
+            <h2>This is Heaven</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+        </div>  
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block img-fluid" src="https://images.pexels.com/photos/7097/people-coffee-tea-meeting.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="banner-text">
+            <h2>This is Heaven</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+        </div>  
+    </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block img-fluid" src="https://images.pexels.com/photos/872957/pexels-photo-872957.jpeg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="banner-text">
+            <h2>This is Heaven</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+        </div>  
+    </div>
+  </div>
+            </div>     
+        
+    </div>
+  </div>
+</div>
+</section>
 </body>
 
-<script>
+   <script>
   if (window.history.replaceState) {
     window.history.replaceState(null, null, window.location.href);
   }
 </script>
-
 </html>
